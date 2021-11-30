949c6b88a6dcd22d029c8e68389b7e1a212ba586
raghav-f2k <93242271+raghav-f2k@users.noreply.github.com>
2021-11-30 15:57:09 +0530

    Back-end

---
+++ RTodos/RTodos/Models/TodoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RTodos.Models
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options): base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
